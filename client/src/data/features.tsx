da1395ba2870783910d614053ed56ae86473e35e
Aditi Shukla <your-email@example.com>
2026-02-04 00:36:42 +0530

    Day 1

---
+++ client/src/data/features.tsx
@@ -0,0 +1,21 @@
+import type { IFeature } from "../types";
+
+export const featuresData: IFeature[] = [
+    {
+        icon: "/assets/thumb_5.jpg",
+        title: "Smart Analysis",
+        description: "Analyzes video content to suggest the most clickable concepts.",
+    },
+     {
+        icon: "/assets/thumb_6.jpg",
+        title: "Designs",
+        description: "Generate vibrant , high - contrast thumbnails that stand out in that feed.",
+    },
+     {
+        icon: "/assets/thumb_7.jpg",
+        title: "Editable",
+        description: "Get fully layered designs you can tweak to perfection ",
+    },
+    
+        
+];
